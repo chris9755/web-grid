3c7e22ddb4c71c8516c0b43bd06078b0858231e7
Krzysztof <gipson.hose@gmail.com>
2023-04-30 11:19:51 +0200

    add menu mobile

--- js/main.js
+++ js/main.js
@@ -15,28 +15,19 @@ window.addEventListener("scroll", ()=>{
 const mobileNav = document.querySelector('.nav-items')
 const navBtn = document.querySelector('.nav-btn');
 
-
-
-
 navBtn.addEventListener("click", () => {
   mobileNav.classList.toggle('active');
   navBtn.classList.toggle('active');
 }
 )
 
-// const nav = document.querySelector('.nav-items')
 
-// document.addEventListener('click', () => {
-//   if (nav.classList.contains('active1')) {
-//     nav.classList.remove('active1')
-//   }
+
+
+// document.querySelectorAll('.nav-items').addEventListener('click', () => {
+//   navBtn.classList.remove('active');
+//   mobileNav.classList.remove('active');
 // })
-
-
-document.querySelectorAll('.nav-items').forEach(n => n.addEventListener('click', () => {
-  navBtn.classList.remove('active');
-  mobileNav.classList.remove('active');
-}))
 
 
 
