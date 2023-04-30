e8dc7828df8db98bc05c84241dd448e50e9b6b23
Krzysztof <gipson.hose@gmail.com>
2023-04-30 17:52:58 +0200

    add menu mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // to-top
 const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", ()=>{
-  if (window.pageYOffset > 1200){
+  if (window.pageYOffset > 1500){
     toTop.classList.add("active");
   } else{
     toTop.classList.remove("active");
