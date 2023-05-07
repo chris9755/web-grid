e192916e92fbc94894d359b4018d51968bbc6781
Krzysztof <gipson.hose@gmail.com>
2023-05-07 21:47:47 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -21,6 +21,17 @@ navBtn.addEventListener("click", () => {
 }
 )
 
+
+
+const navItems = document.querySelectorAll('.nav-items');
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+      navBtn.classList.remove('active');
+      mobileNav.classList.remove('active');
+  });
+});
+
 // document.onclick = function(e){
 //   if('.nav-items'.contains(e.target) && '.nav-btn'.contains(e.target))
 //   mobileNav.classList.remove('active');
